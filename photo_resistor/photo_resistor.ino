6d64699587823aa4c3144d60232240a983d8f33b
tkhag78 <tkhag78@gmail.com>
2025-02-01 20:04:33 +0200

    Added in reset, but it killed things

--- photo_resistor/photo_resistor.ino
+++ photo_resistor/photo_resistor.ino
@@ -8,14 +8,15 @@ int ledPin = 11;  // initialize digital pin 11, connected with LED
 int leddly = 8;  // initialize digital pin 11, connected with LED
 int val = 0;  // initialize variable val to store the sensor value
 int ton = 0;
-
+int pin2input = 0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);  // set digital pin 11 as output
   pinMode(leddly, OUTPUT);  // set digital pin 8 as output
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);  // set baud rate at 9600
-   myservo.write(90);
+  myservo.write(90);
+  pinMode(2, INPUT_PULLUP);
 
   
 }
@@ -32,24 +33,29 @@ void loop() {
   if (val > 350) {  // change the threshold value based on your specific lighting conditions
     digitalWrite(ledPin, LOW);  // turn off the LED if it's too bright
     ton = ++ton;
-
   } 
+
   else {
     digitalWrite(ledPin, HIGH); 
     
     if (ton > 500) {  // change the threshold value based on your specific lighting conditions
-    ton = 499;
-  } 
+      ton = 499;
+    } 
   
    if (ton > 0) {  // change the threshold value based on your specific lighting conditions
     ton = --ton;
-  } 
+    } 
   else {
     ton = 0;
-    
-  }
+ 
+    }
   }
    
+
+  pin2input =  digitalRead(2); 
+  if(pin2input = 0){
+   myservo.write(90);
+   }
 
 
 
@@ -60,8 +66,7 @@ if (ton > 500) {  // change the threshold value based on your specific lighting 
   } 
   else {
     digitalWrite(leddly, HIGH); 
-
-    
+   
   }
-}
 
+}
