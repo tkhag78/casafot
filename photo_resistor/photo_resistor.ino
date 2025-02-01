40c9ff82505a78cfc65ee0da1e6fc17a7cff6c16
tkhag78 <tkhag78@gmail.com>
2025-02-01 20:09:54 +0200

    cleaned and fixed but of == in the lock

--- photo_resistor/photo_resistor.ino
+++ photo_resistor/photo_resistor.ino
@@ -6,19 +6,19 @@ Servo myservo;
 int photoResistorPin = A0;  // initialize analog pin A0, connected with photoresistor
 int ledPin = 11;  // initialize digital pin 11, connected with LED
 int leddly = 8;  // initialize digital pin 11, connected with LED
+int relockPin = 2;  // initialize digital pin 11, connected with LED
 int val = 0;  // initialize variable val to store the sensor value
 int ton = 0;
-int pin2input = 0;
+int relockInput = 0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);  // set digital pin 11 as output
   pinMode(leddly, OUTPUT);  // set digital pin 8 as output
+  pinMode(relockPin, INPUT_PULLUP); // Set digital pin 2 as input with weak pull-up
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  Serial.begin(9600);  // set baud rate at 9600
-  myservo.write(90);
-  pinMode(2, INPUT_PULLUP);
-
-  
+  Serial.begin(9600);  // set baud rate at 9600 - serial debugging
+  myservo.write(90); // Push the servo to the lock position
+   
 }
 
 
@@ -26,7 +26,7 @@ void loop() {
 
 
   val = analogRead(photoResistorPin);  // read the value from the photoresistor and assign it to val
-  Serial.println(val);  // print the value for debugging
+//  Serial.println(val);  // print the value for debugging
 
   delay(10);  // wait for 0.01 seconds
 
@@ -52,8 +52,10 @@ void loop() {
   }
    
 
-  pin2input =  digitalRead(2); 
-  if(pin2input = 0){
+  relockInput =  digitalRead(relockPin); 
+  Serial.println(relockInput);  // print the value for debugging
+
+  if(relockInput == 0){
    myservo.write(90);
    }
 
